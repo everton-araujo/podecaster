077dda4081f8306f3620479a9c7c90af7db622cf
Everton <everton.aaraujo@outlook.com>
2021-04-28 18:55:25 -0300

    Player context

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -53,7 +53,7 @@ export default function Episode({ episode }: EpisodeProps) {
         <span>{episode.durationAsString}</span>
       </header>
 
-      <div 
+      <div
         className={styles.description}
         dangerouslySetInnerHTML={{ __html: episode.description}}
       />
@@ -62,8 +62,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  const paths = data.map((episode) => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  });
+  
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
 }
